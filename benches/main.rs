d48ba95e9da002f518beb4775ed55b6f1bc83d11
Wouter Geraedts <git@woutergeraedts.nl>
2019-08-15 16:20:43 +0200

    Added benches

---
+++ benches/main.rs
@@ -0,0 +1,38 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use waters::*;
+
+fn criterion_benchmark(criterion: &mut Criterion) {
+    let mut rng = rand::thread_rng();
+
+    let id = "email:w.geraedts@sarif.nl".as_bytes();
+    let kid = Identity::derive(id);
+
+    let m = Message::generate(&mut rng);
+
+    let (pk, sk) = setup(32, &mut rng);
+    let usk = extract(&pk, &sk, &kid, &mut rng);
+
+    let c = encrypt(&pk, &kid, &m, &mut rng);
+
+    criterion.bench_function("generate message", |b| {
+        let mut rng = rand::thread_rng();
+        b.iter(|| Message::generate(&mut rng))
+    });
+    criterion.bench_function("setup", |b| {
+        let mut rng = rand::thread_rng();
+        b.iter(|| setup(32, &mut rng))
+    });
+    criterion.bench_function("derive", move |b| b.iter(|| Identity::derive(id)));
+    criterion.bench_function("extract", move |b| {
+        let mut rng = rand::thread_rng();
+        b.iter(|| extract(&pk, &sk, &kid, &mut rng))
+    });
+    criterion.bench_function("encrypt", move |b| {
+        let mut rng = rand::thread_rng();
+        b.iter(|| encrypt(&pk, &kid, &m, &mut rng))
+    });
+    criterion.bench_function("decrypt", move |b| b.iter(|| decrypt(&usk, &c)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
