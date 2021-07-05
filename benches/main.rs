77620ec8c52bf623dbf3dba7eaf8b2fed8f18853
Leon <l.botros@cs.ru.nl>
2021-07-05 16:37:55 +0200

    wip: Boyens & Waters scheme

--- benches/main.rs
+++ benches/main.rs
@@ -113,10 +113,48 @@ fn criterion_kiltz_vahlis_one_benchmark(criterion: &mut Criterion) {
     });
 }
 
+fn criterion_boyens_waters_benchmark(criterion: &mut Criterion) {
+    use ibe::boyens_waters::*;
+
+    let mut rng = rand::thread_rng();
+
+    let id = "email:w.geraedts@sarif.nl".as_bytes();
+    let kid = Identity::derive(id);
+
+    let (pk, sk) = setup(&mut rng);
+    let usk = extract_usk(&pk, &sk, &kid, &mut rng);
+    let ppk = pk.to_bytes();
+
+    let (c, _k) = encrypt(&pk, &kid, &mut rng);
+
+    criterion.bench_function("boyens_waters unpack_pk", |b| {
+        b.iter(|| PublicKey::from_bytes(&ppk))
+    });
+    criterion.bench_function("boyens_waters setup", |b| {
+        let mut rng = rand::thread_rng();
+        b.iter(|| setup(&mut rng))
+    });
+    criterion.bench_function("boyens_waters derive", move |b| {
+        b.iter(|| Identity::derive(id))
+    });
+    criterion.bench_function("boyens_waters extract", move |b| {
+        let mut rng = rand::thread_rng();
+        b.iter(|| extract_usk(black_box(&pk), black_box(&sk), black_box(&kid), &mut rng))
+    });
+    criterion.bench_function("boyens_waters encrypt", move |b| {
+        let mut rng = rand::thread_rng();
+        b.iter(|| encrypt(black_box(&pk), black_box(&kid), &mut rng))
+    });
+    criterion.bench_function("boyens_waters decrypt", move |b| {
+        b.iter(|| decrypt(black_box(&usk), black_box(&c)))
+    });
+}
+
 criterion_group!(
     benches,
     criterion_waters_benchmark,
     criterion_waters_naccache_benchmark,
     criterion_kiltz_vahlis_one_benchmark,
+    criterion_boyens_waters_benchmark,
 );
 criterion_main!(benches);
